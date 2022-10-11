4438c505131212744388062efc5e0017d3217e52
Wagão - ( Wagner Goulart ) <wagner.goulart@neon.com.br>
2022-10-10 22:01:13 -0300

    Initial Commit

---
+++ todo/todoApp.swift
@@ -0,0 +1,17 @@
+//
+//  todoApp.swift
+//  todo
+//
+//  Created by Wagner Barboza Goulart on 10/10/22.
+//
+
+import SwiftUI
+
+@main
+struct todoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
